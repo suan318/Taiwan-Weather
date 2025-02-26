23c713ae4e97e7e4278ecac7c3f0cd874d75e56d
suan318 <yixuanwang0129@gmail.com>
2025-02-26 13:33:28 +0800

    調整圖表樣式

--- src/components/TemperatureChart.js
+++ src/components/TemperatureChart.js
@@ -25,7 +25,13 @@ ChartJS.register(
 const TemperatureChart = ({ data }) => {
 
     // 解析後端數據
-    const labels = data.map((item) => item.date); // X 軸標籤（日期）
+
+    // X 軸標籤（日期）
+    const labels = data.map((item) => {
+        const dateObj = new Date(item.date);
+        return `${dateObj.getMonth() + 1}/${dateObj.getDate()}`;
+    });
+
     const maxTemps = data.map((item) => Number(item.maxTemp)); // 最大溫度
     const minTemps = data.map((item) => Number(item.minTemp)); // 最小溫度
 
@@ -51,16 +57,65 @@ const TemperatureChart = ({ data }) => {
 
     const options = {
         responsive: true,
+        maintainAspectRatio: false,  //允許根據div大小變化
         plugins: {
-            legend: { position: "top" },
-            title: { display: true, text: "未來 7 天氣溫變化" },
+            legend: {
+                display: false,
+                position: "top",
+                labels: {
+                    color: "rgba(255, 255, 255,0.3)", // 設定圖例顏色
+                    font: {
+                        size: 14
+                    }
+                }
+            },
+            title: {
+                display: false,
+                text: "未來 7 天氣溫變化",
+                color: "rgba(255, 255, 255)", // 設定標題顏色
+                font: {
+                    size: 18,
+                    weight: "bold",
+                }
+            }
         },
         scales: {
-            y: { beginAtZero: false },
-        },
+            x: {
+                ticks: {
+                    color: "rgba(255, 255, 255,0.3)", // X 軸字體顏色（日期）
+                    font: {
+                        size: 16,
+                        weight: "bold",
+                    }
+                }
+            },
+            y: {
+                beginAtZero: false,
+                ticks: {
+                    color: "rgba(255, 255, 255,0.3)", // Y 軸字體顏色（溫度）
+                    font: {
+                        size: 16,
+                        weight: "bold",
+                    },
+                }
+            }
+        }
     };
 
-    return <Line data={chartData} options={options} />;
+    // 背景色
+    const chartContainerStyle = {
+        backgroundColor: "rgba(75, 115, 227,0)",
+        padding: "10px",
+        borderRadius: "10px",
+        width: "100%",  // 讓 div 填滿父層
+        height: "400px"// 設定固定高度，確保 canvas 不會塌陷
+    };
+
+    return (
+        <div style={chartContainerStyle}>
+            <Line data={chartData} options={options} />
+        </div>
+    );
 };
 
 export default TemperatureChart;
